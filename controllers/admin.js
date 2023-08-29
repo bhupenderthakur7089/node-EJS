6d0331287013e1fa5368b96222748c74c8f3120e
bhupenderthakur7089 <117012030+bhupenderthakur7089@users.noreply.github.com>
2023-08-29 22:33:11 +0530

    mysql CRUD operations using node

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,10 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save().then(() => {
+    res.redirect('/');
+  })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,17 +57,22 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fileContent]) => {
+      res.render('admin/products', {
+        prods: rows,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.deleteProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.deleteproductbyID(prodId);
-  res.redirect('/admin/products');
+  Product.deleteproductbyID(prodId)
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err))
 };
